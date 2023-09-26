3d7d46e066fdfd34f2b25fd3a6a7ce37a7cc2ad2
Daryl Lee <daryl@daryllee.com>
2023-09-25 21:11:04 -0600

    Finished with 14.6

--- main.c
+++ main.c
@@ -11,11 +11,10 @@ int main(int argc, const char* argv[]) {
     
     // Create an entry in the constant pool and add it to the chunk list 
     int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT);
-    writeChunk(&chunk, constant);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
 
-    // Finish with a "return"
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, OP_RETURN, 123);
 
     // Display the resulting list
     disassembleChunk(&chunk, "test chunk");
