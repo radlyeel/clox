af0be86f7fd4d4a0c5dae62b23531586398ea7de
Daryl Lee <daryl@daryllee.com>
2023-11-09 07:21:21 -0700

    15.2 seems correct now. Ready for 15.3

--- vm.c
+++ vm.c
@@ -5,10 +5,27 @@
 
 VM vm; 
 
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
+
 void initVM() {
+    resetStack();
 }
 
 void freeVM() {
+}
+
+void push(Value value) {
+    //TODO  Change tp *vm.stackTop++ = value
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    //TODO  Change to return *(--vm.stackTop)
+    vm.stackTop--;
+    return *vm.stackTop;
 }
 
 static InterpretResult run() {
@@ -17,6 +34,13 @@ static InterpretResult run() {
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
         disassembleInstruction(vm.chunk, 
                 (int)(vm.ip - vm.chunk->code));
 #endif
@@ -24,11 +48,12 @@ static InterpretResult run() {
         switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
                                   Value constant = READ_CONSTANT();
-                                  printValue(constant);
-                                  printf("\n");
+                                  push(constant);
                                   break;
                               }
             case OP_RETURN: {
+                                printValue(pop());
+                                printf("\n");
                                 return INTERPRET_OK;
                             }
         }
@@ -44,3 +69,4 @@ InterpretResult interpret(Chunk* chunk) {
   vm.ip = vm.chunk->code;
   return run();
 }
+
