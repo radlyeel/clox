697ec9ac0d0a4658b01caef54cf8d21c6756b00c
Daryl Lee <daryl@daryllee.com>
2023-11-09 07:41:44 -0700

    Chapter 15 done; ready for 16

--- vm.c
+++ vm.c
@@ -32,6 +32,14 @@ static InterpretResult run() {
     //This is looking a lot like Forth
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    // Note this trick to scope-control a macro See Sec. 15.3.1
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
+
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
         printf("          ");
@@ -57,11 +65,17 @@ static InterpretResult run() {
                                 return INTERPRET_OK;
                             }
             case OP_NEGATE:   push(-pop()); break;
+            case OP_ADD:      BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE:   BINARY_OP(/); break;
+
         }
     }
 
 #undef READ_CONSTANT
 #undef READ_BYTE
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
