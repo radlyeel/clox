ef1ce00ff3dbf8520c93121f16889053ef0157f1
Daryl Lee <daryl@daryllee.com>
2024-05-27 12:05:13 -0600

    Add line numbering, ready for Ch. 15

--- chunk.h
+++ chunk.h
@@ -12,6 +12,7 @@ typedef enum {
 
 
 typedef struct {
+  // dynmic array for opcodes
   uint8_t* code;
   // These two fields are needed for this to be a dynamic array, like the
   // STL std::vector<uint8_t> in C++
@@ -19,13 +20,16 @@ typedef struct {
   int capacity;
   // Each Chunk has its own constant array
   ValueArray constants;
+  // dynamic array for Line numbers, synchronized with opcodes
+  //    every time we touche 'code', we do the same with 'lines'
+  int *lines;
 
 } Chunk;
 
 // Prototype of initialization function
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int  addConstant(Chunk* chunk, Value value);
 
 #endif
--- main.c
+++ main.c
@@ -5,11 +5,12 @@
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
+    int line = 123;
     int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT);
+    writeChunk(&chunk, OP_CONSTANT, line++);
     // Remember, the chunk contaions a reference to the constant array
-    writeChunk(&chunk, constant);
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, constant, line++);
+    writeChunk(&chunk, OP_RETURN, line++);
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
 
