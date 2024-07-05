bf4256f8a20fec69d7e1c698e8b2b62aeb66bacd
Daryl Lee <daryl@daryllee.com>
2024-07-04 19:26:16 -0600

    WIP Sec. 18.2

--- value.h
+++ value.h
@@ -3,7 +3,31 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef enum {
+    VAL_BOOL,
+    VAL_NIL, 
+    VAL_NUMBER,
+} ValueType;
+
+// Use a tagged union to represent different underlying types.
+typedef struct {
+    ValueType type;
+    union {
+        bool boolean;
+        double number;
+    // This is a commonly-used name for a multipurpose field.
+    } as;
+} Value;
+
+// Sec. 18.2
+
+#define AS_BOOL(value)    ((value).as.boolean)
+#define AS_NUMBER(value)  ((value).as.number)
+
+#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
 
 // For Value arrays, like std::vector<Value>
 typedef struct {
